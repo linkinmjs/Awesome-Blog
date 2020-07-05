6293a8f0566e8afacd8b1bdb21e4c2731a7c3f71
Mauri <linkinmjs@gmail.com>
2020-07-05 18:45:28 -0300

    last commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,43 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`);
+const slash = require(`slash`);
 
-// You can delete this file if you're not using it
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions;
+  // we use the provided allContentfulBlogPost query to fetch the data from Contentful
+  return graphql(
+    `
+      {
+        allContentfulBlogPost {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  ).then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data", result.errors);
+      }
+
+      // Resolve the paths to our template
+      const blogPostTemplate = path.resolve("./src/templates/blogpost.js");
+
+      // Then for each result we create a page.
+      result.data.allContentfulBlogPost.edges.forEach(edge => {
+        createPage({
+          path: `/blogpost/${edge.node.slug}/`,
+          component: slash(blogPostTemplate),
+          context: {
+                        slug: edge.node.slug,
+            id: edge.node.id
+          }
+        });
+      });
+    })
+    .catch(error => {
+      console.log("Error retrieving contentful data", error);
+    });
+};
---
+++ src/pages/blogposts.js
@@ -0,0 +1,49 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BlogPosts = ({ data }) => {
+  const blogPosts = data.allContentfulBlogPost.edges;
+  return (
+    <Layout>
+      <SEO title="Blog posts" />
+            <h1>{"Here's a list of all blogposts!"}</h1>
+      <div className="blogposts">
+        {blogPosts.map(({ node: post }) => (
+          <div key={post.id}>
+            <Link to={`/blogpost/${post.slug}`}>{post.title}</Link>
+          </div>
+        ))}
+        <span className="mgBtm__24" />
+        <Link to="/">Go back to the homepage</Link>
+      </div>
+    </Layout>
+  );
+};
+
+export default BlogPosts;
+
+export const query = graphql`
+  query BlogPostsPageQuery {
+    allContentfulBlogPost(limit: 1000) {
+      edges {
+        node {
+          id
+          title
+          slug
+          body {
+            body
+          }
+          image {
+            file {
+              url
+            }
+          }
+          tags
+        }
+      }
+    }
+  }
+`;
